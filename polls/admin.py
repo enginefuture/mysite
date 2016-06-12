1130c591c4027d8a3fd33c1e9306206d3c2b618c
enginefuture <tabshi@outlook.com>
2016-06-13 00:34:02 +0800

    modified:   db.sqlite3 	modified:   mysite/__init__.pyc 	modified:   mysite/settings.py 	modified:   mysite/settings.pyc 	modified:   mysite/urls.pyc 	modified:   mysite/wsgi.pyc 	modified:   polls/admin.py 	modified:   polls/models.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+    
+
+class QuestionAdmin(admin.ModelAdmin):
+    '''
+        Admin View for QuestionAdmin'''
+    fieldsets = [
+    	('Question information',{'fields':['question_text']}),
+    	('Date information',	{'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+
+
+admin.site.register(Question,QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    """
+    Description: Model Description
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+    	return self.question_text
+
+class Choice(models.Model):
+    """
+    Description: Model Description
+    """
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+    	return self.choice_text
